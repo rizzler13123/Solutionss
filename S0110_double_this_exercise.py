8a9dc62c2a927b7acbe1b865ff7817a446c5857c
rizzler13123 <j4ckp0t@outlook.dk>
2025-04-29 10:40:00 +0200

    ja

---
+++ S0110_double_this_exercise.py
@@ -0,0 +1,40 @@
+"""
+Opgave "double_this":
+
+Som altid skal du læse hele opgavebeskrivelsen omhyggeligt, før du begynder at løse opgaven.
+
+Kopier denne fil til din egen løsningsmappe. Skriv din løsning ind i kopien.
+
+-------
+
+Skriv en funktion med navnet "double_this".
+Scroll ned for at finde det sted i denne fil, hvor du skal skrive funktionen ind.
+
+Funktionen double_this skal ...
+    have én parameter ved navn "number".
+    beregne det dobbelte af værdien af denne parameter og gemme resultatet i en variabel ved navn double_number.
+    returnere resultatet.
+
+Der er allerede én kodelinje i hovedprogrammet, der kalder denne funktion med argumentet 3.
+
+Kør programmet. Tallet 6 bør udskrives i konsollen,
+efterfulgt af "Process finished with exit code 0".
+Det betyder, at programmet sluttede uden en fejlmeddelelse.
+
+-------
+
+Hvis du går i stå, så spørg google, de andre elever, en AI eller læreren.
+
+Når dit program er færdigt, skal du skubbe det til dit github-repository.
+Fortsæt derefter med den næste fil.
+"""
+
+#  Write your function below this line.
+print("Indtast nummer")
+number = int(input())
+
+def double_this():
+    print(number * 2)
+
+# Here starts the main program. From the main program you can call your functions.
+double_this()
